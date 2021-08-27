c7f95de097a3b99cd23ced18aa89235e9e17904d
jcepero <jose.cepero@reign.cl>
2021-08-27 15:49:26 -0400

    update some methods

--- app/src/main/java/com/example/fabanimation/MainActivity.kt
+++ app/src/main/java/com/example/fabanimation/MainActivity.kt
@@ -7,14 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.PathInterpolator
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
-
-    private var menuIsOpen = false
+class MainActivity : AppCompatActivity(), MotionMenu.MenuListener {
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +21,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<MotionMenu>(R.id.motion).setMenuListener(this)
+    }
+
+    override fun onSendClicked() {
+        Toast.makeText(this, "Navigate to Send", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onReceiveClicked() {
+        Toast.makeText(this, "Navigate to Receive", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/fabanimation/MotionMenu.kt
+++ app/src/main/java/com/example/fabanimation/MotionMenu.kt
@@ -1,7 +1,10 @@
 package com.example.fabanimation
 
 import android.content.Context
+import android.media.Image
 import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,8 +16,42 @@ class MotionMenu @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr) {
 
+    private var menuListener: MenuListener? = null
     private var fabMain: FloatingActionButton? = null
+    private var fabSend: ImageView? = null
+    private var fabReceive: ImageView? = null
+    private var overlay: FrameLayout? = null
     private var menuIsOpen = false
+    private var btnClicked = BTNCLICKED.NONE
+
+    private val transitionListener = object : TransitionListener {
+        override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            //
+        }
+
+        override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            //
+        }
+
+        override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            when(btnClicked){
+                BTNCLICKED.SEND -> {
+                    btnClicked = BTNCLICKED.NONE
+                    menuListener?.onSendClicked()
+                }
+                BTNCLICKED.RECEIVE -> {
+                    btnClicked = BTNCLICKED.NONE
+                    menuListener?.onReceiveClicked()
+                }
+                else -> {}
+            }
+        }
+
+        override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            //
+        }
+
+    }
 
     init {
         inflate(context, R.layout.motion_menu, this)
@@ -29,8 +66,16 @@ class MotionMenu @JvmOverloads constructor(
         transitionToStart()
     }
 
+    fun setMenuListener(menuListener: MenuListener) {
+        this.menuListener = menuListener
+    }
+
     override fun onFinishInflate() {
         fabMain = findViewById(R.id.fab)
+        fabSend = findViewById(R.id.fab_1)
+        fabReceive = findViewById(R.id.fab_2)
+        overlay = findViewById(R.id.overlay)
+        addTransitionListener(transitionListener)
         enableClicks()
         super.onFinishInflate()
     }
@@ -42,6 +87,37 @@ class MotionMenu @JvmOverloads constructor(
             } else openMenu()
             menuIsOpen = !menuIsOpen
         }
+
+        overlay?.setOnClickListener {
+            if (menuIsOpen) {
+                closeMenu()
+                menuIsOpen = !menuIsOpen
+            }
+        }
+
+        fabSend?.setOnClickListener {
+            if (menuIsOpen) {
+                btnClicked = BTNCLICKED.SEND
+                closeMenu()
+                menuIsOpen = !menuIsOpen
+            }
+        }
+
+        fabReceive?.setOnClickListener {
+            if (menuIsOpen) {
+                btnClicked = BTNCLICKED.RECEIVE
+                closeMenu()
+                menuIsOpen = !menuIsOpen
+            }
+        }
     }
 
+    private enum class BTNCLICKED {
+        SEND, RECEIVE, NONE
+    }
+
+    interface MenuListener {
+        fun onSendClicked()
+        fun onReceiveClicked()
+    }
 }
